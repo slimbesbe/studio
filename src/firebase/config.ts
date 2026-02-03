a4da72d6e0cb5600498dea30be6361306278aa52
Slim <salim.besbes@gmail.com>
2026-02-03 11:01:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8759607191-13325",
+  "appId": "1:101799264836:web:a63d0d563805d5a1b27211",
+  "apiKey": "AIzaSyCWYdFabr2wDZi4oOnc9nO3tXxx_i2idq4",
+  "authDomain": "studio-8759607191-13325.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "101799264836"
+};
