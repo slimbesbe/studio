ed89fe98508928996d11b82c723b9643608e3b49
Slim <salim.besbes@gmail.com>
2026-02-05 21:34:17 +0000

    quand je clic sur nouvelle question sa affiche

--- src/app/admin/questions/new/page.tsx
+++ src/app/admin/questions/new/page.tsx
@@ -24,6 +24,7 @@ import {
 } from 'lucide-react';
 import { Checkbox } from '@/components/ui/checkbox';
 import { useToast } from '@/hooks/use-toast';
+import { Badge } from '@/components/ui/badge';
 import Link from 'next/link';
 
 interface Option {
